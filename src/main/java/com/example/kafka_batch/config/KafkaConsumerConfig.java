b4be965104c4f063a910bf79c20a9b296338c5b8
seokkyu <tjrrb95@gmail.com>
2021-08-02 23:59:23 +0900

    chore: consumer interval, thread

--- src/main/java/com/example/kafka_batch/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/kafka_batch/config/KafkaConsumerConfig.java
@@ -29,7 +29,7 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "member");
-
+        props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, 500_000);
         return props;
     }
 
@@ -44,7 +44,7 @@ public class KafkaConsumerConfig {
         ConcurrentKafkaListenerContainerFactory<String, MemberEvent> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
-
+        factory.setConcurrency(2);
         return factory;
     }
 
